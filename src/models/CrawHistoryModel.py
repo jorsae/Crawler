ac50050a4c4613fd98a4e684786bccbe72de9b76
jorgen saeter <jorgen.sater@gmail.com>
2021-04-21 13:48:32 +1000

    Added CrawlHistoryModel

--- src/models/CrawHistoryModel.py
+++ src/models/CrawHistoryModel.py
@@ -6,7 +6,11 @@
         - FKEY(Websites.ID)
 """
 from peewee import *
-from models import BaseModel
+from models import BaseModel, DomainModel
 
 class CrawHistoryModel(BaseModel):
-    domain_id = AutoField()
+    crawl_history_id = AutoField()
+    url = TextField()
+    status_code = SmallIntegerField()
+    date_crawled = DateTimeField(null=True)
+    domain_id = ForeignKeyField(DomainModel, to_field='domain_id')
