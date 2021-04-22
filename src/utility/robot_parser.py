69410f10a6f3a1b4fc83f13f9ed7849e522a70d8
jorgen saeter <jorgen.sater@gmail.com>
2021-04-23 00:46:04 +1000

    Completed RobotParse with test for allow

--- src/utility/robot_parser.py
+++ src/utility/robot_parser.py
@@ -8,19 +8,27 @@ class RobotParser():
         self.robot_text = self.robot_text.lower()
         apply_to_this = True
         for line in self.robot_text.split('\n'):
-            if line.startswith('user-agent'):
+            if line.startswith('user-agent:'):
+                line = self.parse_clean(line, 'user-agent:')
                 apply_to_this = self.handle_user_agent(line)
-            elif line.startswith('disallow'):
-                print('disallow')
-            elif line.startswith('allow'):
-                print('allow')
+            elif apply_to_this is False:
+                continue
+            elif line.startswith('disallow:'):
+                line = self.parse_clean(line, 'disallow:')
+                self.disallow.append(line)
+            elif line.startswith('allow:'):
+                line = self.parse_clean(line, 'allow:')
+                self.allow.append(line)
             else:
                 print(f'{line=}')
     
+    def parse_clean(self, line, start):
+        if line.startswith(start):
+            return line[len(start):].strip()
+        else:
+            return None
+    
     def handle_user_agent(self, line):
-        line = line.replace('user-agent:', '')
-        line = line.strip()
-        # TODO: Have this support a name
         if '*' in line:
             return True
         else:
--- test/utility/test_robot_parser.py
+++ test/utility/test_robot_parser.py
@@ -2,10 +2,19 @@ import pytest
 import sys
 sys.path.append('src')
 from utility import *
+import os
 
 def read_file(filename):
-    pass
+    return open(f'test/utility/test-data/{filename}', 'r').read()
 
-def test():
-    rp = RobotParser('robot text')
+testdata_parse_allow = [
+    ('vg_robots.txt', []),
+    ('reddit_robots.txt', ['/', '/sitemaps/*.xml', '/posts/*'])
+    ]
+
+@pytest.mark.parametrize("file, expected", testdata_parse_allow)
+def test_parse_allow(file, expected):
+    data = read_file(file)
+    rp = RobotParser(data)
     rp.parse()
+    assert(rp.allow) == expected
