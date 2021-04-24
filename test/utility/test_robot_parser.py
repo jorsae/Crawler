9d8f88ca00022fecf4ef2ad715ed2d247371ad7c
jorgen saeter <jorgen.sater@gmail.com>
2021-04-24 16:25:03 +1000

    Fixed test method name

--- test/utility/test_robot_parser.py
+++ test/utility/test_robot_parser.py
@@ -11,7 +11,7 @@ testdata_parse_allow = [
     ('vg_robots.txt', []),
     ('reddit_robots.txt', ['/', '/sitemaps/*.xml', '/posts/*']),
     ('test_robots.txt', ['/pleasebethere'])
-    ]
+]
 @pytest.mark.parametrize("file, expected", testdata_parse_allow)
 def test_parse_allow(file, expected):
     data = read_file(file)
@@ -23,7 +23,7 @@ testdata_parse_disallow = [
     ('vg_robots.txt', ['/tegneserier/salesposter', '/poll']),
     ('reddit_robots.txt', ['/r/*/comments/*/*/c*', '/r/*/user/', '/gold?', '/static/button/button1.js', '/static/button/button1.html', '/CAPS']),
     ('test_robots.txt', ['?please=123'])
-    ]
+]
 @pytest.mark.parametrize("file, expected", testdata_parse_disallow)
 def test_parse_disallow(file, expected):
     data = read_file(file)
@@ -35,9 +35,9 @@ testdata_parse_crawl_delay = [
     ('vg_robots.txt', 0),
     ('reddit_robots.txt', 120),
     ('test_robots.txt', 400)
-    ]
+]
 @pytest.mark.parametrize("file, expected", testdata_parse_crawl_delay)
-def test_parse_disallow(file, expected):
+def test_parse_crawl_delay(file, expected):
     data = read_file(file)
     rp = RobotParser(data)
     rp.parse()
