e42f60d307500e0a2f4c4b5583755444ad23a2e7
jorgen saeter <jorgen.sater@gmail.com>
2021-04-25 16:39:04 +1000

    added frame for spider.crawl

--- src/spider.py
+++ src/spider.py
@@ -1,5 +1,6 @@
 import requests
 import logging
+from bs4 import BeautifulSoup
 
 from utility import RobotParser
 
@@ -7,6 +8,10 @@ class Spider():
     def __init__(self, domain):
         self.domain = domain
         self.rp = None
+        self.crawled_urls = 0
+        self.run = True
+        self.queue = set()
+        self.queue.add(f'https://{self.domain}')
     
     def test(self):
         if self.rp is not None:
@@ -15,11 +20,36 @@ class Spider():
             print(self.rp.crawl_delay)
 
     def crawl(self):
-        req = requests.get(f'https://{self.domain}')
-        print(req.text)
-
+        while self.queue and self.run:
+            url = self.queue.pop()
+            req = self.request(url)
+            if req is None:
+                logging.info(f'Request is None: {url}')
+                continue
+            
+            soup = BeautifulSoup(req.text)
+            # TODO: Parse data
+    
+    def request(self, url):
+        self.crawled_urls += 1
+        
+        try:
+            req = requests.get(url, timeout=3)
+        except requests.exceptions.Timeout as timeout:
+            logging.debug(f'Request timed out: {url}')
+            return None
+        except Exception as e:
+            logging.warning(f'Request exception: {e} | {url}')
+            return None
+        
+        # TODO: Add to crawl history
+        return req
+    
     def crawl_robots(self):
         req = requests.get(f'https://{self.domain}/robots.txt')
         if req.status_code == 200:
             self.rp = RobotParser(req.text)
             self.rp.parse()
+        else:
+            # TODO: Do something if you can't find robots.txt
+            pass
